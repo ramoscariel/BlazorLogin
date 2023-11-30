88810ad44d4a75e0176c8e7b0abda5de39946e2b
ramoscariel <103594586+ramoscariel@users.noreply.github.com>
2023-11-29 21:44:36 -0500

    .

---
+++ Client/Extensiones/AutenticacionExtension.cs
@@ -0,0 +1,53 @@
+﻿using BlazorLogin.Shared;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorLogin.Client.Extensiones
+{
+    public class AutenticacionExtension : AuthenticationStateProvider
+    {
+        private readonly ISessionStorageService _sessionStorage;
+        private ClaimsPrincipal _sinInfor = new ClaimsPrincipal(new ClaimsIdentity());
+        public AutenticacionExtension(ISessionStorageService _sessionStorage)
+        {
+            this._sessionStorage = _sessionStorage;
+        }
+
+        public async Task ActualizarEstadoAutenticacion(SesionDTO? sesionUsuario) 
+        {
+            ClaimsPrincipal claimsPrincipal;
+            if(sesionUsuario != null) 
+            {
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+                    new Claim(ClaimTypes.Email,sesionUsuario.Correo),
+                    new Claim(ClaimTypes.Role,sesionUsuario.Rol)
+                },"JwtAuth"));
+                await _sessionStorage.GuardarStorage("sesionUsuario", sesionUsuario);
+            }
+            else 
+            {
+                claimsPrincipal = _sinInfor;
+                await _sessionStorage.RemoveItemAsync("sesionUsuario");
+            }
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var sesionUsuario = await _sessionStorage.ObtenerStorage<SesionDTO>("sesionUsuario");
+            if (sesionUsuario == null) 
+            {
+                return await Task.FromResult(new AuthenticationState(_sinInfor));
+            }
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+                    new Claim(ClaimTypes.Email,sesionUsuario.Correo),
+                    new Claim(ClaimTypes.Role,sesionUsuario.Rol)
+                }, "JwtAuth"));
+            return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+        }
+    }
+}
---
+++ Server/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BlazorLogin.Shared;
+
+namespace BlazorLogin.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login) 
+        {
+            SesionDTO sesionDTO = new SesionDTO();
+            if (login == null)
+            {
+                throw new ArgumentNullException(nameof(login));
+            }
+            if (login.Correo == "admin@gmail.com" && login.Clave == "admin") 
+            {
+                sesionDTO.Nombre = "Administrador";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Administrador";
+            }
+            else 
+            {
+                sesionDTO.Nombre = "Usuario";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Empleado";
+            }
+            return StatusCode(StatusCodes.Status200OK, sesionDTO);
+        }
+    }
+}
